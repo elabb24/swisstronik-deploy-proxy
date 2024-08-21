3dddc2209a87dbe859f260d45302d1b1f5e34296
elabb24 <166648807+elabb24@users.noreply.github.com>
2024-08-21 23:27:15 +0700

    Update deployed-address.ts

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x60Bff502623d8b96917EfE294070f146DA0FdAAb'
+export const ProxyAdmin = '0x1979B46fD3E99e1b15AeC3a78317F70aab5b6bDB'
+export const SWTRImplementation = '0xF1812eC94267b137F5F8Bc6B0bCB52C904dE6d56'
     
